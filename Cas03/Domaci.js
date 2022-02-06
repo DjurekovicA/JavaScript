91da72c8cfb05b9fc0e2adda7cbc20ee0508b247
DjurekovicA <94486399+DjurekovicA@users.noreply.github.com>
2022-02-06 17:26:28 +0100

    Domaci

--- Cas03/Domaci.js
+++ Cas03/Domaci.js
@@ -9,24 +9,42 @@
 
 var niz = [4, 5, 7, 2, 5];
 // 1. nacin
-for (i = 0; i < niz.length; i++)
+for (i = 0; i < niz.length; i++) {
     console.log(niz[i]);
+} 
 
+function ispis(value) {
+    console.log(value);
+}
 // 2. Od prosledjenjog niza brojeva ispisuje samo parne
 for (i = 0 ; i < niz.length ; i++) {
     if (niz[i] % 2 == 0)
         console.log(niz[i]);
 }
 
+function parni(value) {
+    if( value % 2 == 0)
+        console.log(value);
+}
 // 3. Racuna sumu prosledjenog niza
 var suma = 0;
 for(i = 0 ; i < niz.length ; i++){
     suma += niz[i];
 }
 
+var Suma = 0;
+function sum(value) {
+    Suma += value;
+}
 //4. Ispisuje koliko ima brojeva vecih od prvog elementa niza
-var brEl=0;
+var brEl = 0;
 for(i = 0 ; i < niz.length ; i++) {
     if(niz[i]>niz[0])
         brEl++;
+}
+
+var brE = 0;
+function BrojEl(value){
+    if (value > value[0])
+        brE++;
 }
